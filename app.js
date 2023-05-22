23c4f1ef21904dd83a017dbca8c9816bb9579385
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-05-22 16:28:09 -0300

    Practic

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 const {inquirerMenu, pause} = require("./menu/inquirer")
+const Tarea = require("./model/tarea")
+const Tareas = require("./model/tareas")
 
 const main = async () =>{
     console.clear()
     let output = ""
    
     while(output.option !=="0"){
-        output = await inquirerMenu()
+        // output = await inquirerMenu()
+        const tarea = new Tarea("Comprar comida");
+        const tareas = new Tareas()
+        tareas._listado[tarea.id] =tarea
+        console.log(tarea)
+        console.log(tareas)
         await pause()
         if(output.option !=="0"){
             console.log(output)
